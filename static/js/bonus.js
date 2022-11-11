2a4ef675555cf0ca72cbe64310221357cc4965ec
Shaun <gotocooling@gmail.com>
2022-11-12 03:07:03 +1100

    the needle still have some bug

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,24 +1,26 @@
 // Advanced Challenge Assignment (Optional)
 // Calc needle pointer
-function gaugePointer(washFreq){
+function gaugePointer(wfreq){
 	
     radians = (degrees) => degrees * Math.PI / 180;
-    
+    var washFreq = parseInt(wfreq);
     var freqCalc = washFreq / 9 * 180;
-    var degrees = 180 - freqCalc, radius = 0.25; 
+    var degrees = 180 - freqCalc, radius = 0.15; 
 
     const width = 0.01;
-    const half = 0.5;
+    const yoffset = 0.52;
+    const xoffset= 0.50;
 
     // Reference https://observablehq.com/@arronhunt/building-a-gauge-meter-with-plotly
     // x, y is the needle pointer
-    var x  = half + Math.cos(radians(freqCalc)) * radius * -1, // -1 inverts the direction of the rotation
-        y  = half + Math.sin(radians(freqCalc)) * radius;
+    var x  = xoffset + Math.cos(radians(freqCalc)) * radius * -1, // -1 inverts the direction of the rotation
+        y  = yoffset + Math.sin(radians(freqCalc)) * radius;
     // x0, y0 and x1, y1 is the baseline of needle
-    var x0 = half + Math.cos(radians(freqCalc - 90)) * width * -1,
-        y0 = half + Math.sin(radians(freqCalc - 90)) * width,
-        x1 = half + Math.cos(radians(freqCalc - 90)) * width,
-        y1 = half + Math.sin(radians(freqCalc - 90)) * width * -1;
+    var x0 = xoffset + Math.cos(radians(freqCalc - 90)) * width * -1,
+        y0 = yoffset + Math.sin(radians(freqCalc - 90)) * width,
+        x1 = xoffset+ Math.cos(radians(freqCalc - 90)) * width,
+        y1 = yoffset+ Math.sin(radians(freqCalc - 90)) * width * -1;
+    console.log(x0,y0,x1,y1);
     // SVG draw a triangle: M move to point-1 x/y, L from x/y draw a line to point-2 x0/y0, L draw next line from x0,y0 to point-3 x1,y1, and Z will back to point-1 x,y
     var path = `
     M ${x} ${y}
@@ -35,7 +37,7 @@ function gaugeChart(inputValue){
     // make a pie chart, and set half of pie to white, so dispeared
     var data = [
         { values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
-        rotation: 90,
+        rotation: 90, 
         text:['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1'],
         textinfo: 'text',
         textposition:'inside',	  
@@ -61,11 +63,11 @@ function gaugeChart(inputValue){
             }
             }],
         title: '<b>Bully Button Washing Frequency</b><br>Scrubs Per Week',
-        autosize:true,
-        xaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]},
-        yaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]}
+        autosize:true, 
+        xaxis: {visible:true,zeroline:true, showticklabels:false,
+                    showgrid: true, range: [-1.1, 1.1]},
+        yaxis: {visible:true,zeroline:true, showticklabels:false,
+                    showgrid: true, range: [-1.1, 1.1]}
     };
     config={responsive:true};
     // plot the gauge chart
