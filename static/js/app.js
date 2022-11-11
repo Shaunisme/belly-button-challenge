4d0cb40762fde54caa3165acceab6f18c3574beb
Shaun <gotocooling@gmail.com>
2022-11-12 01:26:23 +1100

    app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
-//1.Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
-
+//1.Use the D3 library to read in samples.json from the URL
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 var dataSet;
 
